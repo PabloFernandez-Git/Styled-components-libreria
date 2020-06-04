fabaa870ce4c54f68e4594ed12770419f0ad1c86
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-06-04 17:35:43 -0300

    Nuestro primer Styled Component

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,4 @@ class App extends Component {
 
 export default App;
 
-/*
-Styled components es una librería para dar estilos a componentes.
-Nos permite hacer estilos dinámicos de una forma muy sencilla.
-Hace mas fácil el mantenimiento del CSS.
-No sustituye a CSS, la teoría es la misma.
 
-https://styled-components.com/
-
-
-Instalar styled-components solo toma una simple linea de commando y ya estas listo para trabajar:
-
-npm i styled-components
-
-(Debemos ejecutar este comando dentro de la carpeta del proyecto)
-
-
-*/
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,63 @@
 import React from 'react'
+//import './button.css'
+import styled from 'styled-components'
 
-import './button.css'
+
+const StyledButton = styled.button`
+    background-color: royalblue;
+    color: #fff;
+
+    &:hover{
+        background-color: red;
+        color: royalblue;
+    }
+`
 
 const Button = () => {
     return (
-        <button className='button'>Click Me!</button>
+        <StyledButton>Click Me!</StyledButton>
     )
 }
 
 export default Button
+
+
+/*
+Creando nuestro primer Styled Component
+
+Extension recomendada:
+vscode-styled-components
+
+1. Importar la libreria
+
+import styled from 'styled-components'
+
+
+2. Crear una constante que es donde se va a guardar nuestro styled component.
+Todos los styled components deben ir guardados en constantes y se tratan como componentes, por lo tanto empiezan con mayuscula.
+
+const StyledButton = styled.button`
+    background-color: royalblue;
+    color: #fff;
+
+    &:hover{
+        background-color: red;
+        color: royalblue;
+    }
+`
+
+La constante que estamos creando (StyledButton) va a ser = a 'styled' (la libreria) seguida de un punto (.) y el elemento que queremos crear.
+
+const StyledButton = styled.button
+
+A continuacion, sin espacios, colocamos `` para crear un template string.
+Dentro del template string se trabajaba igual que en CSS/SASS
+
+
+3. Ahora para utilizar nuestro styled component debemos utilizarlo como etiqueta dentro de nuestro codigo:
+
+<StyledButton>Click Me!</StyledButton>
+
+
+
+*/
