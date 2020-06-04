a112951e4303b6be5e36266ffc19566aec98a8ad
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-06-04 18:42:32 -0300

    Variables

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 //import './button.css'
 import styled from 'styled-components'
 
+const color = 'royalblue'
+const padding = '2rem'
 
 const StyledButton = styled.button`
-    background-color: royalblue;
+    background-color: ${color};
     color: #fff;
+    padding: ${padding};
 
     &:hover{
         background-color: red;
-        color: royalblue;
+        color: ${color};
     }
 `
 
@@ -23,40 +26,19 @@ export default Button
 
 
 /*
-Creando nuestro primer Styled Component
+Variables
 
-Extension recomendada:
-vscode-styled-components
+Con Styled components podemos crear cualquier variable:
 
-1. Importar la libreria
+const color = 'royalblue'
+const padding = '2rem'
 
-import styled from 'styled-components'
+Para utilizar esa variable lo unico que tenemos que hacer es interpolarla exactamente igual que en un template string:
 
-
-2. Crear una constante que es donde se va a guardar nuestro styled component.
-Todos los styled components deben ir guardados en constantes y se tratan como componentes, por lo tanto empiezan con mayuscula.
-
-const StyledButton = styled.button`
-    background-color: royalblue;
-    color: #fff;
-
-    &:hover{
-        background-color: red;
-        color: royalblue;
-    }
-`
-
-La constante que estamos creando (StyledButton) va a ser = a 'styled' (la libreria) seguida de un punto (.) y el elemento que queremos crear.
-
-const StyledButton = styled.button
-
-A continuacion, sin espacios, colocamos `` para crear un template string.
-Dentro del template string se trabajaba igual que en CSS/SASS
+background-color: ${color};
+padding: ${padding};
 
 
-3. Ahora para utilizar nuestro styled component debemos utilizarlo como etiqueta dentro de nuestro codigo:
-
-<StyledButton>Click Me!</StyledButton>
 
 
 
