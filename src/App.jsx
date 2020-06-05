2f8dac9ca07c3479f8d0dbe9b02ecdbe34963a89
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-06-05 19:20:25 -0300

    Estilos globales

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import React, { Component } from 'react'
 import Title from './components/Title';
 import Button from './components/Button';
 
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
 import colors from './theming/colors'
+
+
+const GlobalStyle = createGlobalStyle`
+    body{
+        background-color: greenyellow;
+    }
+`
 
 class App extends Component {
 
     render() {
         return (
             <>
+                <GlobalStyle />
                 <Title />
                 <ThemeProvider theme={colors}>
                     <Button>Click Me!</Button>
@@ -23,3 +31,43 @@ class App extends Component {
 }
 
 export default App;
+
+
+/*
+Estilos globales
+
+A. Podemos aplicar estilos globales con un 'index.css' importado en 'index.js' (el archivo que renderiza toda la aplicacion).
+
+
+B. Con 'styled-components' tenemos una forma de aplicar estilos globales sin depender de un archivo CSS/SASS externo.   
+
+1. En nuestro 'App.jsx' tenemos que importar 'createGlobalStyle'.
+Este es el componente que nos da 'styled-components' para crear los estilos globales.
+
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+
+Funciona exactamente igual que cualquier 'styled-components'.
+
+2. Crear el objeto ('styled-components') con los estilos
+
+const GlobalStyle = createGlobalStyle`
+    body{
+        background-color: greenyellow;
+    }
+`
+
+3. Al ser un componente solo resta utilizarlo.
+No necesitamos ambas etiquetas; usamos una etiqueta que se cierra a si misma.
+
+<GlobalStyle />
+<Title />
+<ThemeProvider theme={colors}>
+    <Button>Click Me!</Button>
+    <Button danger>Click Me!</Button>
+    <Button ghost>Ghost Me!</Button>
+    <Button danger ghost>Ghost Me!</Button>
+</ThemeProvider>
+
+
+
+*/
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,54 +1,11 @@
 import React from 'react'
 
-import styled from 'styled-components'
-
-const getBorder = (width, type, color) => {
-    return `${width} ${type} ${color}`
-}
-
-const Header = styled.header`
-    border: ${getBorder('3px', 'solid', 'royalblue')};
-    text-align: center;
-    margin-bottom: 1rem;
-
-    & h1{
-        margin: 0;
-        color:red;
-    }
-
-    & h1::after{
-        content:'';
-        display: block;
-        width:100px;
-        height:100px;
-        background-color: greenyellow;
-    }
-
-    &:hover ::after{
-        background-color: blue;
-    }
-`
-
 const Title = () => {
     return (
-        <Header>
-            <h1>
-                Styled Components
-             </h1>
-        </Header >
+        <h1>
+            Styled Components
+        </h1>
     )
 }
 
 export default Title
-
-
-/*
-Anidamiento
-
-En casos donde tenemos un elemento dentro de otro, por ejemplo un <h1> dentro de un <header>, podemos anidar los elementos utilizando un '&'. 
-
-Dependera la cantidad de codigo (css) que requiera cada elemento si conviene anidarlos o crear un nuevo elemento y luego anidar las etiquetas.
-
-
-
-*/
